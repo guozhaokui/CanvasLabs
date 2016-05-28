cea8d28b5461c5faab0c8598b735675ea4730c12
guozhaokui <guozk@layabox.com>
2016-05-28 22:10:26 +0800

    .

---
+++ labs/apps/DAtlas/DAtlas.ts
@@ -0,0 +1,38 @@
+///<reference path="../../runtime/defination/gl-matrix.d.ts" />
+'use strict'
+import FPS2D = require('../../runtime/runtimeMod/common/FPS2D');
+
+function startAnimation(renderFunc: () => void) {
+    function _render() {
+        renderFunc();
+        window.requestAnimationFrame(_render);
+    }
+    window.requestAnimationFrame(_render);
+}
+
+var updateFPS = new FPS2D.FPS2D().updateFPS;
+
+class DAtlas {
+    canv: HTMLCanvasElement = null;
+    ctx: CanvasRenderingContext2D = null;
+    constructor(canv: HTMLCanvasElement) {
+        this.canv = canv;
+        this.ctx = canv.getContext("2d");
+    }
+
+    onRender = () => {
+        var ctx = this.ctx;
+        var img = new Image();
+        img.src='a.png';
+        ctx.drawImage(img,0,0);
+        
+        
+                
+        updateFPS(this.ctx);
+    }
+}
+
+export function main(canv: HTMLCanvasElement) {
+    var app = new DAtlas(canv);
+    startAnimation(app.onRender);
+}
--- labs/apps/webglLine/webglLine.ts
+++ labs/apps/webglLine/webglLine.ts
@@ -25,40 +25,31 @@ class testWebglLine {
     gpuprog = new gpuProg.GpuProgram();
     mesh = new mesh.Mesh();
     renderGroup = new renderer.RenderGroup();
-    
     texture:WebGLTexture = null;
-    
     rundataDesc=new NamedData();        //rundata描述
     rundata:MyRunData = null;         //rundata实例
     material = new material.Material();
     rundatas: ArrayBuffer[] = [null, null];
-    
-    eyePosFinal = new Float32Array([0, 0, -2]);
-    targetPosFinal = new Float32Array([0, 0, 1]);
-    upPosFinal = new Float32Array([0, 1, 0]);
-    
-    resok: boolean = false;
-    //camera: Camera = new Camera(this.eyePosFinal, this.targetPosFinal, this.upPosFinal);
+    gl:WebGLRenderingContext=null;
     matobj:Float32Array = mat4.create();
     matCam:Float32Array = mat4.create();
     
     constructor(canv:HTMLCanvasElement) {
-        var wgl:WebGLRenderingContext = <WebGLRenderingContext> canv.getContext("webgl", { antialias: false });
-        wgl.viewport(0, 0, canv.width, canv.height);
-        this.init(wgl);
+        this.gl = <WebGLRenderingContext> canv.getContext("webgl", { antialias: false });
+        this.gl.viewport(0, 0, canv.width, canv.height);
+        this.init(this.gl);
         
         this.rundataDesc
             .add("g_worldmatrix", 0, NamedData.tp_mat4, 1)
             .add("g_persmat", 64, NamedData.tp_mat4, 1);
         this.rundata = <MyRunData>this.rundataDesc.createDataInstance();
-        
         this.rundatas[0] = this.material.getRunData();
         this.rundatas[1] = this.rundata.getAB();
     }
 
     init(webgl: WebGLRenderingContext) {
         renderer.Renderer.initGlExt(webgl);
-        var gl = <renderer.WebGLExt>webgl;
+        var gl= <renderer.WebGLExt>webgl;
         glMatrix.setMatrixArrayType(Float32Array);
         
         gl.disable(gl.CULL_FACE);
@@ -66,24 +57,32 @@ class testWebglLine {
         //gl.enable(gl.ALPHA_TEST);
         gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA);
         gl.enable(gl.BLEND);
-        mat4.perspective(this.rundata.g_persmat, 3.141593 / 4.0, gl.drawingBufferWidth / gl.drawingBufferHeight, 0.1, 200.0);
-        this.eyePosFinal = new Float32Array([0, 0, 2]);
-        this.targetPosFinal = new Float32Array([0, 0, 0]);
-        this.upPosFinal = new Float32Array([1, 0, 0]);
     }
 
-    onRender(webgl: WebGLRenderingContext) {
-        var gl = <renderer.WebGLExt>webgl;
+    onRender() {
+        var gl =this.gl;
         gl.clearColor(0.8, 0.8, 0.8, 1);
         gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-        if (!this.resok)
-            return;
-
-        gl.renderMesh(this.renderGroup, this.rundatas);
-        window.requestAnimationFrame(()=>{this.onRender(webgl)});
+        gl.lineWidth(10);
+        gl.drawArrays(gl.LINE_STRIP,0,3);
+        //gl.drawArrays(gl.LINE_STRIP,0,4);
+        //gl.renderMesh(this.renderGroup, this.rundatas);
+        
+        render(data,[shader,shaderdata]);
+        
+        drawImage(img){
+            asmBuff();
+            assinMaterial();
+            render(buf,material)
+        }
+        
+        drawImage(img1,x,y);
+        drawImage(img2,x,y);
+        fillRect(rect);
     }
 }
 
 export function main(canv:HTMLCanvasElement){
-    new testWebglLine(canv);    
+    var app =new testWebglLine(canv);   
+    startAnimation(app.onRender.bind(app)); 
 }

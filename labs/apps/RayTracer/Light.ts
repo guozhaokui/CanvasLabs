1f3c8b5cb2ccee160ccdfb0e667cc64d90d6d8f0
guozhaokui <guozk@layabox.com>
2016-11-07 15:14:50 +0800

    tmp

---
+++ labs/apps/RayTracer/Light.ts
@@ -0,0 +1,28 @@
+
+import {Vector3} from '../../runtime/runtimeMod/math/Vector3';
+import {Color} from './Color';
+
+interface LightBase{
+    color:Color;
+}
+
+export class PointLight{
+    constructor(){
+
+    }
+}
+
+export class DirLight{
+    dir=new Vector3(0,0,0);
+    color=new Color(1,1,1);
+    constructor(dir:Vector3,color:Color){
+        this.dir=dir;
+        this.color=color;
+    }
+    /**
+     * 获取某个点的灯光的颜色和朝向
+     */
+    getLight(pos:Vector3,dir:Vector3,color:Color){
+
+    }
+}
---
+++ labs/apps/ocean/sampler.ts
@@ -0,0 +1,11 @@
+
+class Sampler{
+    img:HTMLElement;
+    constructor(img:HTMLImageElement){
+        this.img = img;
+    }
+
+    getV(u:number,v:number):Uint8ClampedArray{
+        return null;
+    }
+}

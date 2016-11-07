8f79835fd319a4317850fc5bbd40139ee4fa2301
guozhaokui <guozk@layabox.com>
2016-11-07 22:32:02 +0800

    可以控制球采样的 上

--- labs/apps/ocean/ocean.ts
+++ labs/apps/ocean/ocean.ts
@@ -167,17 +167,27 @@ export class Ocean{
                 */
                 //球形贴图头顶是z,x朝左，y朝上，
                 //法线在高度上的投影对应uv的v，从-1到1转成0到1
-                var texc_v = (1+outerz)/2;//这个也可以用y，那下面的就用x，z
+                var upisz = false;//北极为z轴
+                var texc_u,texc_v;
+                if( upisz){
+                    texc_v = (1+outerz)/2;//这个也可以用y，那下面的就用x，z
+                    //法线在赤道上的投影转到角度对应的是u坐标。这个投影是(nx,ny,0)
+                    var ang = Math.atan2(outerx,outery);//这个得到的值是 -π到π， 0对应的是x坐标的时候，
+                    //转到0到1。方向颠倒
+                    texc_u = (-ang+π)/2/π;
+                }else{
+                    texc_v = (1+outery)/2;//这个也可以用y，那下面的就用x，z
+                    //法线在赤道上的投影转到角度对应的是u坐标。这个投影是(nx,ny,0)
+                    var ang = Math.atan2(outerx,outerz);//这个得到的值是 -π到π， 0对应的是x坐标的时候，
+                    //转到0到1。方向颠倒
+                    texc_u = (-ang+π)/2/π;
+                }
                 if(false){//texc_v<0.5){
                     // pix[ci++]=0; 
                     // pix[ci++]=0;
                     // pix[ci++]=0;
                     // pix[ci++]=255;//color[3];
                 }else{
-                    //法线在赤道上的投影转到角度对应的是u坐标。这个投影是(nx,ny,0)
-                    var ang = Math.atan2(outerx,outery);//这个得到的值是 -π到π， 0对应的是x坐标的时候，
-                    //转到0到1。方向颠倒
-                    var texc_u = (-ang+π)/2/π;
                     //偏移90度，让y指向的为0
                     //先不做。可以通过贴图转换来做
                     this.sky.sample(texc_u,texc_v,color);
